45d35ea8b6f6819a2e589523bae17d6738a2d165
Denomant <denomant.yt@gmail.com>
2023-12-11 13:06:55 +0200

    Append funcion

--- Source/List Test.cpp
+++ Source/List Test.cpp
@@ -1,7 +1,10 @@
 #include "Singly linked List.h"
 
 int main() {
-
+	MyString obj;
+	obj.Append('a');
+	obj.Append('b');
+	obj.Append('c');
 
 
 	return 0;
--- Source/Singly linked List.h
+++ Source/Singly linked List.h
@@ -1,20 +1,38 @@
+#include <iostream>
 
 class CharNode {
 public:
 	char Value;
-	CharNode *NextSymbolptr = nullptr;
+	CharNode *Nextptr = nullptr;
 	CharNode(char Value) {
 		this->Value = Value;
 	}
 };
- 
 
-class MyString() {
+
+class MyString {
 public:
-	CharNode *HeadNode;
+	CharNode *HeadNode = new CharNode('\0');//null
+
+	void Append(char Value) {
+		//Char Version
+		 
+		//First Append
+		if(HeadNode->Value == '\0') {
+			HeadNode->Value = Value;
+			return;
+		}
+
+		//Skip Valued Nodes
+		CharNode *CurrentNode = HeadNode;
+		while(CurrentNode->Nextptr != nullptr) {
+			CurrentNode = CurrentNode->Nextptr;
+		}
+
+		//New Node Set
+		CurrentNode->Nextptr = new CharNode(Value);
+	}
 };
-
-
 
 
 
