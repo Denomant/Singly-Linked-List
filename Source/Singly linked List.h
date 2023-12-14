bc3b1901262aa9875368fcfd196b822664a845e8
Denomant <denomant.yt@gmail.com>
2023-12-14 18:55:09 +0200

    GetChar func

--- Source/Singly linked List.h
+++ Source/Singly linked List.h
@@ -19,24 +19,26 @@ public:
 	void Append(char Value) {
 		//Char Version
 		 
-		//First Append
+		// First Append
 		if(HeadNode->Value == '\0') {
 			HeadNode->Value = Value;
 			return;
 		}
 
-		//Skip Valued Nodes
+		// Skip Valued Nodes
 		CharNode *CurrentNode = HeadNode;
 		while(CurrentNode->Nextptr != nullptr) {
 			CurrentNode = CurrentNode->Nextptr;
 		}
 
-		//New Node Set
+		// New Node Set
 		CurrentNode->Nextptr = new CharNode(Value);
 	}
 
 
 	void Append(MyString *StringToAdd) {
+		//FIXME: if this->HeadNode->Value == '\0'
+
 		//MyString
 				
 		// Non empty MyString
@@ -45,12 +47,23 @@ public:
 		}
 
 		// Skip valued nodes
+		// To change O(n^2) to O(n)
 		CharNode *CurrentNodeToSave = HeadNode;
 		while(CurrentNodeToSave->Nextptr != nullptr) {
 			CurrentNodeToSave = CurrentNodeToSave->Nextptr;
 		}
 
 		CharNode *CurrentNodeToAdd = StringToAdd->HeadNode;
+
+		// if the String that we add to it, is empty
+		if(CurrentNodeToSave->Value == '\0') {
+			CurrentNodeToSave->Value = CurrentNodeToAdd->Value;
+			if(CurrentNodeToAdd->Nextptr == nullptr) {
+				return;
+			}
+			CurrentNodeToAdd = CurrentNodeToAdd->Nextptr;
+		}
+
 		while(CurrentNodeToAdd->Nextptr != nullptr) {
 			CurrentNodeToSave->Nextptr = new CharNode(CurrentNodeToAdd->Value);
 			CurrentNodeToSave = CurrentNodeToSave->Nextptr;
@@ -61,6 +74,28 @@ public:
 
 	}
 
+
+	char Get(int Index) {
+		//char
+
+		// return null if the index is negative
+		if(Index < 0) {
+			return '\0';
+		}
+
+		CharNode *CurrentNode = HeadNode;
+
+		while(Index != 0) {
+			// return null if index out of range
+			if(CurrentNode->Nextptr == nullptr) {
+				return '\0';
+			}
+
+			CurrentNode = CurrentNode->Nextptr;
+			Index -= 1;
+		}
+		return CurrentNode->Value;
+	}
 };
 
 
