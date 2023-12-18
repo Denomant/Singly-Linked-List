647dc2276d4cadb7627b84fd7015a41f483aacb3
Denomant <denomant.yt@gmail.com>
2023-12-18 23:26:53 +0200

    Added [start, end] version for .get() func

--- Source/Singly linked List.h
+++ Source/Singly linked List.h
@@ -37,9 +37,7 @@ public:
 
 
 	void Append(MyString *StringToAdd) {
-		//FIXME: if this->HeadNode->Value == '\0'
-
-		//MyString
+		//MyString Version
 				
 		// Non empty MyString
 		if(StringToAdd->HeadNode->Value == '\0') {
@@ -70,13 +68,11 @@ public:
 			CurrentNodeToAdd = CurrentNodeToAdd->Nextptr;
 		}
 		CurrentNodeToSave->Nextptr = new CharNode(CurrentNodeToAdd->Value);
-
-
 	}
 
 
 	char Get(int Index) {
-		//char
+		//char Version
 
 		// return null if the index is negative
 		if(Index < 0) {
@@ -96,7 +92,62 @@ public:
 		}
 		return CurrentNode->Value;
 	}
+
+
+	MyString Get(int IndexStart, int IndexEnd) {
+		//MyString Version
+		// Includes IndexStart, and does not IndexEnd
+
+		MyString MyStringEmpty;
+		MyString MyStringReturn;
+		
+		// Special cases
+		if(IndexStart == IndexEnd) {
+			MyStringReturn.HeadNode->Value = this->Get(IndexStart);
+			return MyStringReturn;
+		}
+		
+		if(IndexEnd < IndexStart) {
+			return MyStringEmpty;
+		}
+
+		if(IndexStart < 0) {
+			return MyStringEmpty;
+		}
+		
+
+		CharNode *CurrentNodeToAdd = HeadNode;
+		int NodesBeforeIndexStart = IndexStart;
+		
+		//Find IndexStart
+		while(NodesBeforeIndexStart > 0) {
+			//if IndexStart out of range
+			if(CurrentNodeToAdd->Nextptr == nullptr) {
+				
+				return MyStringEmpty;
+			}
+			CurrentNodeToAdd = CurrentNodeToAdd->Nextptr;
+			NodesBeforeIndexStart -= 1;
+			
+		}
+		//CurrentNodeToAdd at IndexStaert
+		
+		CharNode *CurrentNodeToSave = MyStringReturn.HeadNode;
+		int CountCharsToAdd = IndexEnd-IndexStart;
+		
+		while(CountCharsToAdd >= 0) {
+			//if IndexEnd out of range
+			if(CurrentNodeToAdd->Nextptr == nullptr and CountCharsToAdd != 0) {
+				return MyStringEmpty;
+			}
+			
+			CurrentNodeToSave->Value = CurrentNodeToAdd->Value;
+			cout << CurrentNodeToAdd->Value << " Added\n";
+			CurrentNodeToSave->Nextptr = new CharNode('\0');
+			CurrentNodeToSave = CurrentNodeToSave->Nextptr;
+			CurrentNodeToAdd = CurrentNodeToAdd->Nextptr;
+			CountCharsToAdd -= 1;
+		}
+		return MyStringReturn;
+	}
 };
-
-
-
